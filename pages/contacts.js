49970184454c93d293f17880fb737e36e9dd2010
Elaine <elaine@Elaines-MacBook-Air.local>
2024-05-26 10:35:38 +0700

    On branch master  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json
    Committer: Elaine <elaine@Elaines-MacBook-Air.local>  On branch master
    
    Initial commit
    	new file:   .DS_Store
    	new file:   .gitignore
    	new file:   .prettierignore
    	new file:   components/fonts.js
    	new file:   components/layouts/main.js
    	new file:   components/logo.js
    	new file:   components/navbar.js
          	new file:   components/paragraph.js
    	new file:   components/section.js
    	new file:   components/theme-toggle-button.js
      	new file:   eslintrc.json
    	new file:   lib/theme.js
    	new file:   next.config.js
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   pages/_app.js
    	new file:   pages/contacts.js
    	new file:   pages/index.js
    	new file:   pages/uses.js
    	new file:   prettier.config.js

---
+++ pages/contacts.js
@@ -0,0 +1,17 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+
+const Contacts = () => (
+  <Container>
+    <Heading as="h3" fontSize={20} mb={6}>
+      Contacts
+    </Heading>
+    <Paragraph>
+      My only contact is Discord at the moment so DM me at "nerdyelaine". Email
+      coming soon.
+    </Paragraph>
+  </Container>
+)
+
+export default Contacts
